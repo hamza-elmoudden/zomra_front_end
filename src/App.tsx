51d90bcf4c217f1a0a65e4a365365066fbbfe9fc
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-06-15 14:47:01 +0100

    app tex

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import AppRouter from '@/router/AppRouter'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: { retry: 1, staleTime: 30_000 },
-  },
-})
-
 export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <AppRouter />
-    </QueryClientProvider>
-  )
+  return <AppRouter />
 }
